46927f6d423f03e63b2039ccf2935a50a9995783
Stefan Mansson <stefan.aotik@gmail.com>
2020-07-27 08:46:25 +0200

    refactor: first implementation of color schemes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Flex, Text } from 'silicon.ui';
+import { ChromePicker } from 'react-color';
+import { TinyColor } from '@ctrl/tinycolor';
 
 export default function App() {
-    return <p>Color</p>;
+    const [pickerVisible, setPickerVisible] = useState(false);
+    const [baseColor, setBaseColor] = useState('#ff5500');
+    const [darkSettings, setDarkSettings] = useState({
+        spin: 0,
+        darkness: 10,
+        saturate: 10,
+        size: 4,
+    });
+    const [lightSettings, setLightSettings] = useState({
+        spin: 0,
+        lightness: 10,
+        saturate: 10,
+        size: 4,
+    });
+
+    const darkColors: TinyColor[] = Array(darkSettings.size)
+        .fill(baseColor)
+        .reduce((arr, color) => {
+            let newColor = new TinyColor(arr[arr.length - 1] || color);
+
+            Object.entries(darkSettings).forEach(([key, value]) => {
+                switch (key) {
+                    case 'spin':
+                        newColor = newColor.spin(value);
+                        break;
+                    case 'darkness':
+                        newColor = newColor.darken(value);
+                        break;
+                    case 'saturate':
+                        newColor = newColor.saturate(value);
+                        break;
+                }
+            });
+
+            arr.push(newColor);
+            return arr;
+        }, []);
+
+    const lightColors: TinyColor[] = Array(lightSettings.size)
+        .fill(baseColor)
+        .reduce((arr, color) => {
+            let newColor = new TinyColor(arr[arr.length - 1] || color);
+
+            Object.entries(lightSettings).forEach(([key, value]) => {
+                switch (key) {
+                    case 'spin':
+                        newColor = newColor.spin(value);
+                        break;
+                    case 'lightness':
+                        newColor = newColor.lighten(value);
+                        break;
+                    case 'saturate':
+                        newColor = newColor.saturate(value);
+                        break;
+                }
+            });
+
+            arr.push(newColor);
+            return arr;
+        }, []);
+
+    const allColors = [...darkColors.reverse(), new TinyColor(baseColor), ...lightColors];
+
+    return (
+        <>
+            <Flex
+                height={24}
+                width={48}
+                style={{ backgroundColor: baseColor }}
+                onClick={() => setPickerVisible(true)}
+            />
+            <Text>{baseColor}</Text>
+            <Text>Colors</Text>
+
+            {allColors.map((color, index) => (
+                <Flex key={`dark-${index}`} height={24} width={48} style={{ backgroundColor: color.toHexString() }} />
+            ))}
+
+            {pickerVisible && <ChromePicker color={baseColor} onChange={(result) => setBaseColor(result.hex)} />}
+        </>
+    );
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import App from './App';
+import { Silicon } from 'silicon.ui';
 
 export default function Routes() {
     return (
-        <Router>
-            <Switch>
-                <Route exact={true} path="/" component={App} />
-                <Redirect path="*" to="/" />
-            </Switch>
-        </Router>
+        <Silicon>
+            <Router>
+                <Switch>
+                    <Route exact={true} path="/" component={App} />
+                    <Redirect path="*" to="/" />
+                </Switch>
+            </Router>
+        </Silicon>
     );
 }
